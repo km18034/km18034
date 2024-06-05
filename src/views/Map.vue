9c648e7774f2ab7ae5412033ede75cb53618b044
km18034 <kamila99m@gmail.com>
2024-06-05 16:18:42 +0300

    Added new files

---
+++ src/views/Map.vue
@@ -0,0 +1,54 @@
+<script setup>
+import ObjectItem from '@/components/ObjectItem.vue';
+import MapBanner from '@/components/MapBanner.vue';
+import ChipChoice from '@/components/ChipChoice.vue';
+</script>
+
+<template>
+    <main>
+      <MapBanner
+        :background-image="'../../src/assets/img/map_' +  $route.params.category + '.png'"
+      />
+
+      <ChipChoice/>
+
+      <ObjectItem
+        v-for="(object, key) in objects.filter((obj) => obj.category === $route.params.category)"
+        :key="object.id"
+        :object-slug="object.slug"
+        :object-category="object.category"
+        :map-list="true"
+        :title="object.title"
+        :description="object.intro"
+        :image="'../../src/assets/img/' + object.image"
+        :change-order="key%2 !== 0"
+        :path="'../../src/assets/svg/path_' + getPathNumber() + '.svg'"
+      />
+    </main>
+</template>  
+
+<script>
+import sourceData from '@/data.json';
+
+export default {
+  data() {
+    return {
+      objects: sourceData.objects,
+      isMobile: false,
+    }
+  },
+  mounted() {
+    this.isMobile = screen.width <= 760;
+    this.$nextTick(() => {
+      window.addEventListener('resize', () => {
+        this.isMobile = screen.width <= 760;
+      });
+    })
+  },
+  methods: {
+    getPathNumber() {
+      return Math.floor(Math.random() * 4 + 1);
+    },
+  }
+}
+</script>
